74fd749976715b5529a1ffc7faef501e47d48842
Ryan-Solomon <ryanjsolomon24@gmail.com>
2021-01-20 20:06:02 -0600

    display grid of users

--- src/pages/Users.tsx
+++ src/pages/Users.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import styled from 'styled-components';
 import FollowerCard from '../components/FollowerCard';
 import { useFetchUsers } from './../hooks/useFetchUsers';
 
@@ -8,11 +9,18 @@ export default function Users() {
   if (status === 'error' || !users) return <h1>Error</h1>;
 
   return (
-    <>
+    <UsersContainer>
       {users.length > 0 &&
         users.map((user) => {
           return <FollowerCard user={user} key={user.id} />;
         })}
-    </>
+    </UsersContainer>
   );
 }
+
+const UsersContainer = styled.main`
+  display: grid;
+  grid-template-columns: repeat(auto-fit, minmax(250px, 1fr));
+  gap: 2rem;
+  padding: 2rem;
+`;
