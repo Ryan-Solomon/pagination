7905bebffabecd8b7a0b721de4f67ade428ef057
Ryan-Solomon <ryanjsolomon24@gmail.com>
2021-01-20 20:00:13 -0600

    add card styles

--- src/components/FollowerCard.tsx
+++ src/components/FollowerCard.tsx
@@ -1,5 +1,32 @@
-import React from 'react';
+import { TUser } from '../types/types';
+import styled from 'styled-components';
 
-export default function FollowCard() {
+type TProps = {
+  user: TUser;
+};
+
+export default function FollowCard({ user }: TProps) {
   return <h1>Follow</h1>;
 }
+
+const CardContainer = styled.div`
+  background: #fff;
+  display: flex;
+  flex-direction: column;
+`;
+
+const CardImage = styled.img`
+  height: 5rem;
+  width: 5rem;
+  border-radius: 50%;
+  object-fit: cover;
+`;
+
+const CardName = styled.h3`
+  color: black;
+`;
+
+const CardButton = styled.button`
+  background: #444444;
+  color: white;
+`;
