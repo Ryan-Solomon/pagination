d4eac96d791223abd3b0143fe11736aa57156532
Ryan-Solomon <ryanjsolomon24@gmail.com>
2021-01-20 19:53:05 -0600

    render out users to page

--- src/pages/Users.tsx
+++ src/pages/Users.tsx
@@ -3,5 +3,15 @@ import { useFetchUsers } from './../hooks/useFetchUsers';
 
 export default function Users() {
   const { status, users } = useFetchUsers({ howManyUsers: 10, pageNumber: 1 });
-  return <h1>Users</h1>;
+  if (status === 'loading') return <h1>Loading...</h1>;
+  if (status === 'error' || !users) return <h1>Error</h1>;
+
+  return (
+    <>
+      {users.length > 0 &&
+        users.map((user) => {
+          return <h1 key={user.id}>{user.login}</h1>;
+        })}
+    </>
+  );
 }
