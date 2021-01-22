7d7a28d68c145b8176bd55cda10855d5512aaa4a
Ryan-Solomon <ryanjsolomon24@gmail.com>
2021-01-21 18:13:50 -0600

    map over pageArr and render pages

--- src/components/BottomPages.tsx
+++ src/components/BottomPages.tsx
@@ -10,11 +10,18 @@ export default function BottomPages({ pageCount }: TProps) {
     new Array(pageCount).fill(null).map((_, idx) => idx + 1)
   );
   console.log(pagesArray);
-  return <PagesContainer>{}</PagesContainer>;
+  return (
+    <PagesContainer>
+      {pagesArray.map((v, idx) => {
+        return <PageSelector key={v + idx}>{v}</PageSelector>;
+      })}
+    </PagesContainer>
+  );
 }
 
 const PagesContainer = styled.ul`
   display: flex;
+  list-style: none;
 `;
 
 const PageSelector = styled.li`
